ef161f66bbb571c0cd177823ae616499028aa0f6
Jesus-RA <jesus.ra98@hotmail.com>
2020-11-12 16:35:42 -0600

    Add Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,13 +2,13 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id="></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0BC4VD6VBP"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', '');
+        gtag('config', 'G-0BC4VD6VBP');
     </script>
 
     <meta charset="utf-8">
