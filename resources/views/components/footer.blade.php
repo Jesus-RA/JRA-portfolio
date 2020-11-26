d0c655a1f9ec96b99902a20cf6879107a733b890
Jesus-RA <jesus.ra98@hotmail.com>
2020-11-26 10:47:43 -0600

    Fixed contact form bugs and improved design

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,9 +1,12 @@
 <footer class="footer shadow-lg">
     <div class="container py-3">
         <div class="row justify-content-between align-items-center">
-            <div class="col-md-3 text-center ">
+            <div class="col-md-3 d-flex justify-content-center align-items-center">
                 <a href="https://www.linkedin.com/in/jesúsra" target="_blank" class="flaticon-linkedin mx-2 icon"></a>
                 <a href="https://github.com/Jesus-RA" target="_blank" class="flaticon-github mx-2 icon"></a>
+                <a href="mailto:jesus.ra98@hotmail.com">
+                    <img src="{{ secure_asset('img/icons/email.svg') }}" width="30" height="30" class="mx-2 icon">
+                </a>
             </div>
 
             <div class="col-md-4 author">
