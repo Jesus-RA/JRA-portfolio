a902d06802a2a125487f96b69d8d4fd4a8d474e4
Jesus-RA <jesus.ra98@hotmail.com>
2021-01-19 13:08:03 -0600

    Changed the order to display the projects portfolio to descending order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $projects = Project::all();
+
+        $projects = Project::orderBy('created_at', 'desc')->get();
         $technologies = Technology::all();
         $profile = Profile::findOrFail(1);
         
